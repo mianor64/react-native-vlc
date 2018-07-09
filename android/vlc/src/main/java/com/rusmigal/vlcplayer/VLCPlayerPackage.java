c9af97a4c61b695747e633701fb3038094dca196
Romain Lefebvre <mianor64@gmail.com>
2018-07-09 07:41:21 -0400

    Rollback

--- android/vlc/src/main/java/com/rusmigal/vlcplayer/VLCPlayerPackage.java
+++ android/vlc/src/main/java/com/rusmigal/vlcplayer/VLCPlayerPackage.java
@@ -7,7 +7,6 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,9 +14,7 @@ public class VLCPlayerPackage implements ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new VLCPlayer(reactContext));
-        return modules;
+        return Collections.emptyList();
     }
 
     // Deprecated RN 0.47
